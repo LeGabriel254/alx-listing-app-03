db32cf8b10c462601b898ef72b182f4348799ec3
Leon Gabriel <g73346920@gmail.com>
2025-01-30 15:16:30 +0300

    Initial commits

---
+++ alx-listing-app/components/Booking/BookingForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookingForm:React.FC = () => {
+  return (
+    <div>BookingForm</div>
+  )
+}
+
+export default BookingForm
---
+++ alx-listing-app/components/Booking/OrderSummary.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ const OrderSummary:React.FC = () => {
+  return (
+    <div>OrderSummary</div>
+  )
+}
+
+export default OrderSummary;
